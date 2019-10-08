c2e0c16644fafc7c99cff9043cb06ec1bfe77b0f
Kailan483 <kailan457@gmail.com>
2019-10-08 22:53:19 +0300

    Добавлена ещё одна строк кода

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,11 @@
 package com.company;
 
 public class Main {
-    
+
     public static void main(String[] args) {
 	    System.getProperties().list(System.out);
 	    System.out.println(System.getProperty("user.name"));
 	    System.out.println(System.getProperty("java.library.path"));
+	    System.out.println(System.getProperty("user.password"));
     }
 }///:~
