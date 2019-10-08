67d83d07a3d870a5e1d61c2479ab413b8d753611
Kailan483 <kailan457@gmail.com>
2019-10-08 22:50:52 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+//: ShowProperties
+package com.company;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	    System.getProperties().list(System.out);
+	    System.out.println(System.getProperty("user.name"));
+	    System.out.println(System.getProperty("java.library.path"));
+    }
+}///:~
